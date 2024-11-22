d626b740a9ef3a126fa44dbac8bec0ada07c67e2
kirillio25 <this.ionkin.kirill@gmail.com>
2024-11-22 22:44:53 +0500

    add Composite

---
+++ Структурные/composite/fileManager/Component.php
@@ -0,0 +1,8 @@
+<?php
+
+interface Component
+{
+    public function getName(): string;
+    public function getSize(): int; // Возвращает размер элемента (0 для папок, либо размер файла)
+    public function countFiles(): int; // Количество файлов в компоненте
+}
